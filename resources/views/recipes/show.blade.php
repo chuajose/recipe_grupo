32ce45559c7dfe27c453c7a30a7e3c27f6607796
Jose Manuel Suarez Bravo <chua.jose@gmail.com>
2020-04-06 23:00:43 +0200

    init

--- resources/views/recipes/show.blade.php
+++ resources/views/recipes/show.blade.php
@@ -88,7 +88,7 @@
                 <div class="row">
                     <div class="col-12 col-lg-8">
                         <!-- Single Preparation Step -->
-                        <div class="single-preparation-step d-flex">
+                        <div class="single-preparation-step d-flex-no">
                             {!! $recipe->description !!}
                         </div>
                         <!-- Single Preparation Step -->
